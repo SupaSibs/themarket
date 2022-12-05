e9a8d025cadc3b00fc1c0744efc3746370dc4ff5
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-12-05 05:15:08 +0000

    breakpoints and new text\

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { component$, useStyles$ } from '@builder.io/qwik';
 import globalStyles from '../global.css?inline';
-import Nav from "../components/navbar/main.tsx
+import Nav from "../components/navbar/main"
 export default component$(() => {
 useStyles$(globalStyles)
 return (
@@ -8,8 +8,13 @@ return (
 <Nav />
 <main>
 <section>
-<h1 class="uppercase text-2xl 2xs:text-5xl text-black">
+<h1 class="uppercase text-2xl 2xs:text-3xl sm:text-5xl text-black">
   The marketplace for everyone.
+</h1></section>
+
+  <section>
+<h1 class="uppercase text-xl 2xs:text-3xl text-black">
+  Find what you want at the cheapest price 
 </h1></section>
 </main>
   </>
