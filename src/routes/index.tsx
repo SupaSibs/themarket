a31be9f1cd5473e50655cc933e0685b3f6d892cc
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-12-06 16:04:58 +0000

    fix section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,12 +10,10 @@ return (
 <section>
 <h1 class="uppercase text-2xl 2xs:text-3xl sm:text-5xl text-black">
   The marketplace for everyone.
-</h1></section>
-
-  <section>
-<h1 class="uppercase text-xl 2xs:text-3xl text-black">
-  Find what you want at the cheapest price 
-</h1></section>
+</h1>
+<p class=" text-xl 2xs:text-2xl text-black">
+  Find what you want at the cheapest price.
+</p></section>
 </main>
   </>
 )
