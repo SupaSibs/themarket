6d41ca1b084459c827529a504f51e8577f7e2a40
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-12-08 05:01:41 +0000

    centerized

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,17 @@
 import { component$, useStyles$ } from '@builder.io/qwik';
-import globalStyles from '../global.css?inline';
-import Nav from "../components/navbar/main"
+import globalStyles from '/src/global.css?inline';
+import Nav from "/src/components/navbar/main"
 export default component$(() => {
 useStyles$(globalStyles)
 return (
   <>
 <Nav />
 <main>
-<section>
-<h1 class="uppercase text-2xl 2xs:text-3xl sm:text-5xl text-black">
+<section class="text-center">
+<h1 class="uppercase text-2xl 2xs:text-3xl xs:text-5xl text-black opacity-100 ">
   The marketplace for everyone.
 </h1>
-<p class=" text-xl 2xs:text-2xl text-black">
+<p class=" text-xl 2xs:text-2xl text-gray-500 opacity-80">
   Find what you want at the cheapest price.
 </p></section>
 </main>
