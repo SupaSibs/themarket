f398c2f73eed7665222d01c86736e8434e73ac9b
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-12-11 05:14:41 +0000

    dfsdfsdfsd

--- src/components/navbar/main.tsx
+++ src/components/navbar/main.tsx
@@ -4,7 +4,7 @@ import globalStyles from '../../global.css?inline';
 export default component$(() => {
 useStyles$(globalStyles)
 return (
-<nav class="flex justify-evenly flex-row items-center bg-blue-700 shadow-md" >
+<nav class="flex justify-evenly flex-row items-center bg-blue-700 shadow-md " >
 <ul>
 <li>
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,15 +7,50 @@ return (
   <>
 <Nav />
 <main>
-<section class="text-center">
+<section class="text-center mb-7 mt-1">
 <h1 class="uppercase text-2xl 2xs:text-3xl xs:text-5xl text-black opacity-100 ">
-  The marketplace for everyone.
+  The marketplace for everyone
 </h1>
-<p class=" text-xl 2xs:text-2xl text-gray-500 opacity-80">
+<p class=" text-xl 2xs:text-2xl text-gray-800 opacity-80">
   Find what you want at the cheapest price.
-</p></section>
+</p>
+<a href="#">
+<button class="uppercase bg-sky-300">Sign up today</button></a></section>
+  <section class="text-center my-7">
+<h1 class="uppercase text-2xl 2xs:text-3xl xs:text-5xl  opacity-100 ">
+  Get deals like never before
+</h1>
+<p class=" text-xl 2xs:text-2xl text-gray-800 opacity-80">
+ Get the best deals, automatically, no fuss.
+</p>
+  <a href="#">
+<button class="uppercase">Sign up today</button></a></section>
+  <section class="text-center my-7">
+<h1 class="uppercase text-2xl 2xs:text-3xl xs:text-5xl text-black opacity-100 ">
+  Earn free cashback
+</h1>
+<p class=" text-xl 2xs:text-2xl text-gray-800 opacity-80">
+Earn up to 15% cashback with each purchase
+</p>
+  <a href="#">
+<button class="uppercase">Sign up today</button></a></section>
+    <section class="text-center my-7">
+<h1 class="uppercase text-2xl 2xs:text-3xl xs:text-5xl text-black opacity-100 ">
+  Never pay more
+</h1>
+<p class=" text-xl 2xs:text-2xl text-gray-800 opacity-80">
+Never pay extra for your products.
+</p>
+    <a href="#">
+<button class="uppercase">Sign up today</button></a></section>
 </main>
   </>
 )
 	})
 
+/*
+cache information
+Link button:
+<a href="#">
+<button class="uppercase">Sign up today</button></a>
+*/
