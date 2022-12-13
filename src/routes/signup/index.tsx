9f3f3ee3444d0694d60436f5dfd0b6a58ff9e92c
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-12-13 06:52:34 +0000

    sdfsdfsdfsdsdf

--- src/routes/signup/index.tsx
+++ src/routes/signup/index.tsx
@@ -4,17 +4,36 @@ import globalStyles from '/src/global.css?inline';
 export default component$(() => {
 useStyles$(globalStyles)
   return (
-    <form action="" method="POST">
+    <main class="fillpage flex items-center justify-center">
+    <form class="bg-orange-400 p-6 rounded flex flex-col shadow" action="" method="POST">
+      <p class="text-center">Sign up</p>
       <input
+        
+        class="bg-black text-white my-6 block"
         id="username" 
         name="username"></input>
+     
             <input
+        class="bg-black text-white my-6 block"
         id="email" 
         name="email"></input>
+     
             <input
+         class="bg-black text-white my-6 block"
         id="password" 
         name="password"></input>
+     
+      <input
+        class="btn"
+        type="submit"></input>
     </form>
+    </main>
   )
   
 })
+
+/*
+     span::after {
+       content: '\a';
+       white-space: pre;
+   }*/
